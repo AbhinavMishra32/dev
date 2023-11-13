291e53f2095cd9a6da33eae1d3f5d5eb0eedaac9
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2023-11-13 19:06:22 +0530

    update

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,5 +2,23 @@
 // array = A + ""
 // array = array.replaceAll(",", ' ');
 // console.log(array);
+let logicArray = [];
+globalLogicString = "";
+function solveFunction(clickedButton){
+  userInput = clickedButton.innerHTML;
+  if(userInput != "C"){
+    logicArray.push(userInput);  
+  }
+  logicString = logicArray.toString().replaceAll(",", "");
 
+  console.log(logicArray);
+  document.querySelector('.display').innerHTML = logicString;
+  globalLogicString = logicString;
+  //return logicStringEvaluated;
+  
+}
 
+function showAnswer(){
+	logicStringEvaluated = eval(globalLogicString);
+	document.querySelector('.display').innerHTML = logicStringEvaluated;
+}
