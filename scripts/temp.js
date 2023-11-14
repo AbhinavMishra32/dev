d767722570ecd7fa834e156ffefd2ef6cad173c3
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-14 22:28:45 +0530

    update

--- scripts/temp.js
+++ scripts/temp.js
@@ -1,33 +1,29 @@
 function countMatched(nArray) {
-  
-let symbolCountArr = []
+  let symbolCountArr = [];
+
   for (let i = 0; i < nArray.length; i++) {
-    let symbolOcur = 0;
       for (let j = 0; j < nArray[i].length; j++) {
           const currentSymbol = nArray[i][j];
-          
+          let symbolOcur = 0;
+
           if (i > 0 && nArray[i - 1][j] === currentSymbol) { // Check top
               symbolOcur++;
-              continue;
           }
 
           if (i < nArray.length - 1 && nArray[i + 1][j] === currentSymbol) { // Check bottom
               symbolOcur++;
-              continue;
           }
 
           if (j > 0 && nArray[i][j - 1] === currentSymbol) { // Check left
               symbolOcur++;
-              continue;
           }
 
           if (j < nArray[i].length - 1 && nArray[i][j + 1] === currentSymbol) { // Check right
               symbolOcur++;
-              continue;
           }
-		  symbolCountArr.push({ [currentSymbol]: symbolOcur });
+
+          symbolCountArr.push({ [currentSymbol]: symbolOcur });
       }
-	  
   }
 
   return symbolCountArr;
@@ -35,10 +31,10 @@ let symbolCountArr = []
 
 const nestedArray = [
   ['a', '*', '*', '*', '*'],
-  ['b', 'c', '*', 'd', 'e'],
+  ['f', 'c', '*', 'd', 'e'],
   ['f', 'g', '*', '*', 'h'],
   ['*', '*', '*', 'i', 'j'],
   ['k', 'l', 'm', 'n', 'o']
 ];
 
-console.log(countMatched(nestedArray)); // Output: 12
+console.log(countMatched(nestedArray));
