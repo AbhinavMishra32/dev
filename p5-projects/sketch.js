ed19bed49b028e5e55e08b58e087f3869e087c0c
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-17 13:52:17 +0530

    update

--- p5-projects/sketch.js
+++ p5-projects/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
+	// background(0);
 }
 
 function draw() {
-  background(220);
+    background(0)
+    fill(mouseX)
+    circle(mouseX, mouseY, 50)
 }
