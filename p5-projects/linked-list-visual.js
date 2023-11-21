ea88972cad3e9dff2637b27273a2b4b13fcf542e
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-21 22:18:23 +0530

    update

--- p5-projects/linked-list-visual.js
+++ p5-projects/linked-list-visual.js
@@ -1,25 +1,51 @@
+let ball;
 function setup(){
     createCanvas(400, 400);
+    ball = new Ball(200, 200, 10);
 
 }
 
 function draw(){
     background(30);
+    ball.show();
+    ball.update();
+    // if(keyPressed === true){
+    //     ball.applyForce([0, 0.1]);
+    // }
+
 }
 
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        ball.applyForce([0, -0.1]);
+    }
+    if(keyCode === DOWN_ARROW){
+        ball.applyForce([0, 0.1]);
+    }
+    if(keyCode === LEFT_ARROW){
+        ball.applyForce([-0.1, 0]);
+    }
+    if(keyCode === RIGHT_ARROW){
+        ball.applyForce([0.1, 0]);
+    }
 
+}
+
+const G = 6.67408e-11;
+const accEarth = 9.8;
 
 class Ball{
-    constructor(x, y){
+    constructor(x, y, mass){
+        
         this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        this.mass = mass;
     }
 
     update(){
-        g = 
         this.pos.add(this.vel);
         this.vel.add(this.acc);
-        this.
     }
 
     show(){
@@ -29,6 +55,6 @@ class Ball{
     }
 
     applyForce(force){
-        this.vel.add(force);
+        this.acc.add(force);
     }
 }
