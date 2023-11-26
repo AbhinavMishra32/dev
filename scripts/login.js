6e7f1afdea36eda3825de031d90fc6aaf05298e7
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-26 18:33:47 +0530

    Update login page design

---
+++ scripts/login.js
@@ -0,0 +1,33 @@
+accounts = {
+    "admin": "admin123",
+    "Abhinav": "abhinav235",
+    "Shubham": "shubham890",
+    "Shivam": "shivam2u9"
+}
+
+// function validate() {
+//     var username = document.getElementById("username").value;
+//     var password = document.getElementById("password").value;
+
+//     if (accounts[username] == password) {
+//         window.location = "index.html";
+//         return false;
+//     } else {
+//         alert("Login failed. Please try again.");
+//     }
+// }
+
+
+function validate(){
+    let username = document.querySelector(".username-field").value;
+    let password = document.querySelector(".password-field").value;
+
+    if(accounts[username] == password){
+        alert("logged in"); 
+    }
+    else{
+        alert("wrong username or password");
+    }
+    document.querySelector(".password-field").value = "";
+}
+
